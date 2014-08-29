8405c6ed3d880c7313feea602baabf65da27e6e0
Dave Richmond <daverichmond@gmail.com>
2014-08-29 11:24:19 +0200

    Initial commit

---
+++ RF_Cascade_Learn/RF_Cascade_Learn.cxx
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <algorithm>
+#include <math.h>
+#include <ctime>
+
+#include <vigra/impex.hxx>
+#include <vigra/multi_impex.hxx>
+#include <vigra/multi_array.hxx>
+
+#include <vigra/random_forest.hxx>
+#include <vigra/random_forest/rf_earlystopping.hxx>
+//#include <vigra/random_forest/rf_visitors.hxx>
+
+#include <vigra/hdf5impex.hxx>
+#include <vigra/random_forest_hdf5_impex.hxx>
+
+// #include <omp.h>
+
+using namespace vigra;
+
+int main(int argc, char ** argv)
+{
+
+    // for now, manually define some useful constants
+    int num_classes = 2; // could pass in as argument, or extract from array_labels;
+    int num_levels = atoi(argv[3]);
+    int num_samples_per_level = 2;
+
+	// set up clock for timing
+	std::clock_t start;
+	float duration;
+	
+	// import images --------------------->
+	
+    /* CALL FUNCTION HERE THAT ACCEPTS "MODE" OF USING GT DATA, AND RETURNS:
+     * (1) ARRAY OF FEATURE_ARRAYS,
+     * (2) ARRAY OF LABEL_ARRAYS
+     */
+
+    // import train, label and test images
+    ImageImportInfo train_features_info(argv[1]);
+    ImageImportInfo train_labels_info(argv[2]);
+
+    // create images of appropriate size
+    MultiArray<2, float> train_features(Shape2(train_features_info.shape()));
+    MultiArray<2, UInt8> train_labels(Shape2(train_labels_info.shape()));
+
+    // import the images just read
+    importImage(train_features_info, train_features);
+    importImage(train_labels_info, train_labels);
+
+    // import RF options ----------------------->
+
+    // import feature_mix
+    MultiArray<int> feature_mix(3);
+    feature_mix[0] = atoi(argv[7]);
+    feature_mix[1] = atoi(argv[8]);
+    feature_mix[2] = atoi(argv[9]);
+
+    // set early stopping depth
+    int depth = atoi(argv[11]);
+    int min_split_node_size = atoi(argv[12]);
+    EarlyStopDepthAndNodeSize stopping(depth, min_split_node_size);
+
+    // calc some more useful constants ----------------------->
+
+    int num_train_samples = train_features.size(0);
+    int num_filt_features = train_features.size(1);
+    int num_all_features = num_filt_features + 2*num_classes;
+
+    std::cout << "num_train_samples: " << num_train_samples << std::endl;
+    std::cout << "num_features: " << num_all_features << std::endl;
+
+    // set up arrays of data ------------------------->
+
+    MultiArray< MultiArray<2, float> > array_train_features(1);
+    MultiArray< MultiArray<2, UInt8> > array_train_labels(1);
+
+    for (int i=0; i<num_levels; ++i){
+        (array_train_features[i]).reshape(Shape2(num_train_samples,num_all_features));
+        (array_train_features[i]).subarray(Shape2(0,0), Shape2(num_train_samples,num_filt_features)) = train_features;
+
+        array_train_labels[i] = train_labels;
+    }
+
+    // set up array of RFs --------------------->
+
+    MultiArray< RandomForest<float> > array_rf(num_levels);
+
+    // learn cascade --------------------------------->
+
+    // define probs to store output of predictProbabilities
+    MultiArray<2, float> probs(Shape2(num_train_samples, num_classes));
+    MultiArray<2, float> smooth_probs(Shape2(num_train_samples, num_classes));
+
+    // tic
+    start = std::clock();
+
+    // run cascade
+    for (int i=0; i<num_levels; ++i)
+    {
+        // generate prob maps for ith forest (from previous (i-1) forests)
+        for (int j=0; j<i; ++j)
+        {
+            // generate new probability map
+            if (j==0)
+                (array_rf[j]).predictProbabilities((array_train_features[i]).subarray(Shape2(0,0), Shape2(num_train_samples,num_filt_features)), probs);    // there are no probs from previous rf, therefore don't look into this part of feature array
+            else
+                (array_rf[j]).predictProbabilities(array_train_features[i], probs);
+
+            // smooth the new probability map
+            smooth_probs = probs;       // FOR NOW, NO SMOOTHING
+
+            // update train_features with current probability map, and smoothed probability map
+            (array_train_features[i]).subarray(Shape2(0,num_filt_features), Shape2(num_train_samples,num_filt_features+num_classes)) = probs;
+            (array_train_features[i]).subarray(Shape2(0,num_filt_features+num_classes), Shape2(num_train_samples,num_all_features)) = smooth_probs;
+        }
+
+        // learn ith forest
+        (array_rf[i]).set_options().image_shape(Shape2(atoi(argv[4]),atoi(argv[5])))
+                             .tree_count(atoi(argv[6]))
+                             .use_stratification(RF_EQUAL)
+                             .max_offset_x(atoi(argv[10]))
+                             .max_offset_y(atoi(argv[10]))
+                             .feature_mix(feature_mix);
+
+        if (i==0)
+            (array_rf[i]).learn((array_train_features[i]).subarray(Shape2(0,0), Shape2(num_train_samples,num_filt_features)), array_train_labels[i], rf_default(), rf_default(), stopping);
+        else
+            (array_rf[i]).learn(array_train_features[i], array_train_labels[i], rf_default(), rf_default(), stopping);
+    }
+
+    // toc
+	duration = (std::clock() - start) / (float) CLOCKS_PER_SEC;
+    std::cout << "time to learn cascade: " << duration << std::endl;
+	
+//    // SAVE LEARNED RFs
+
+//    HDF5File hdf5_file(argv[3], HDF5File::New);
+//    rf_export_HDF5(rf, hdf5_file);
+
+}
---
+++ supporting_code/imagetools.hxx
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <array>
+#include <chrono>
+#include <algorithm>
+#include <math.h>
+#include <ctime>
+
+#include <vigra/random.hxx>
+#include <vigra/impex.hxx>
+#include <vigra/multi_impex.hxx>
+#include <vigra/multi_array.hxx>
+#include <vigra/multi_convolution.hxx>
+
+using namespace vigra;
+
+class imagetools
+{
+public:
+
+	template <class T1, class T2> 
+	static void imageToFeatures(const MultiArray<3, T1> & image, const MultiArray<2, T2> & labels, MultiArray<2, T1> & rfFeatures, MultiArray<2, T2> & rfLabels, int downSample = 0, float downSampleFraction = 0.05)
+	{
+		// calc some useful constants
+        int num_samples = image.size(0)*image.size(1);
+		int num_features = image.size(2);
+        int count = 0;
+        int num_used_samples = 0;
+
+		// 
+        if (downSample)
+		{
+			// initialize
+            MultiArray<2, T1> rfFeatures_temp;
+            MultiArray<2, T2> rfLabels_temp;
+            rfFeatures_temp.reshape(Shape2(num_samples, num_features));
+            rfLabels_temp.reshape(Shape2(num_samples, 1));
+
+            MersenneTwister random;
+			
+			for (int j = 0; j<image.size(1); ++j) {
+				for (int i = 0; i<image.size(0); ++i) {
+					if (labels(i, j) == 0) {
+						if (random.uniform() <= downSampleFraction) {
+                            rfLabels_temp(count, 0) = labels(i, j);
+							for (int k = 0; k<num_features; ++k) {
+                                rfFeatures_temp(count, k) = image(i, j, k);
+							}
+                            ++count;
+						}
+					}
+					else {
+                        rfLabels_temp(count, 0) = labels(i, j);
+						for (int k = 0; k<num_features; ++k) {
+                            rfFeatures_temp(count, k) = image(i, j, k);
+						}
+                        ++count;
+					}
+				}
+			}
+            num_used_samples = count;
+
+            // allocate memory and transfer from 'temp' arrays
+            rfFeatures.reshape(Shape2(num_used_samples,num_features));
+            rfFeatures = rfFeatures_temp.subarray(Shape2(0,0),Shape2(num_used_samples,num_features));
+
+            rfLabels.reshape(Shape2(num_used_samples,1));
+            rfLabels = rfLabels_temp.subarray(Shape2(0,0),Shape2(num_used_samples,1));
+        }
+        else {
+            // all samples are used
+            num_used_samples = num_samples;
+            rfFeatures.reshape(Shape2(num_used_samples, num_features));
+            rfLabels.reshape(Shape2(num_used_samples, 1));
+
+            //
+            for (int j = 0; j<image.size(1); ++j) {
+                for (int i = 0; i<image.size(0); ++i) {
+                    rfLabels(count, 0) = labels(i, j);
+                    for (int k = 0; k<num_features; ++k) {
+                        rfFeatures(count, k) = image(i, j, k);
+                    }
+                    ++count;
+                }
+            }
+		}		
+	}
+
+	template <class T1, class T2>
+    static void imagesToFeatures(const MultiArray<1, MultiArray<3, T1> > & imageArray, const MultiArray<1, MultiArray<2, T2> > & labelArray, MultiArray<2, T1> & rfFeatures, MultiArray<2, T2> & rfLabels, int downSample = 0, float downSampleFraction = 0.05)
+	{
+		// calc some useful constants
+        int num_images = imageArray.size(0);
+		if (!num_images) return;
+
+        int num_samples_per_image = imageArray(0).size(0)*imageArray(0).size(1);
+        int num_samples = num_images*num_samples_per_image;
+        int num_features = imageArray(0).size(2);
+
+        rfFeatures.reshape(Shape2(num_samples, num_features));
+        rfLabels.reshape(Shape2(num_samples, 1));
+
+        for (int imgIdx = 0; imgIdx < num_images; imgIdx++) {
+            MultiArray<2, T1> rfFeaturesPerImage = rfFeatures.subarray(Shape2(imgIdx*num_samples_per_image, 0), Shape2((imgIdx + 1)*num_samples_per_image, num_features));
+            MultiArray<2, T2> rfLabelsPerImage = rfLabels.subarray(Shape2(imgIdx*num_samples_per_image, 0), Shape2((imgIdx + 1)*num_samples_per_image, 1));
+            imageToFeatures<T1, T2>(imageArray(imgIdx), labelArray(imgIdx), rfFeaturesPerImage, rfLabelsPerImage, downSample, downSampleFraction);
+		}
+	}
+
+	template <class T1, class T2>
+    static void featuresToImage(MultiArray<2, T1> & rfFeatures, MultiArray<2, T2> & rfLabels, const MultiArray<3, T1> & image, const MultiArray<2, T2> & labels, Shape2 & image_shape)
+	{
+        image.reshape(Shape3(image_shape(0), image_shape(1), rfFeatures.size(2)));
+        labels.reshape(image_shape);
+
+        // consistency check
+        if (image.shape(0)*image.shape(1) != rfFeatures.size(0))
+        {
+            std::cout << "image shape is inconsistent with dimensions of rfFeatures" << std::endl;
+            exit(-1);
+        }
+
+        int count = 0;
+        for (int j = 0; j<image.size(1); ++j) {
+            for (int i = 0; i<image.size(0); ++i) {
+                labels(i,j) = rfLabels(count,0);
+                for (int k = 0; k<image.size(2); ++k) {
+                    image(i,j,k) = rfFeatures(count,k);
+                }
+                ++count;
+            }
+        }
+
+	}
+
+    template <class T1, class T2>
+    static void featuresToImages()
+    {
+
+    }
+
+	template <class T2> 
+	static float dice(MultiArray<2, T2> & groundTruthLabels, MultiArray<2, T2> & autoLabels)
+	{
+		// float diceScore = 2*volume_of_intersection/(volume_1+volume_2);
+		// return diceScore;
+	}
+
+};
