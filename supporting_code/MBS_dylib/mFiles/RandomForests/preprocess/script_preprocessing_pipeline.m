19795d635dc1da125b71b9b9effb5a226b4306f0
Dave Richmond <daverichmond@gmail.com>
2014-11-08 19:13:32 +0100

    add gt label generation to pre-processing pipeline

--- supporting_code/MBS_dylib/mFiles/RandomForests/preprocess/script_preprocessing_pipeline.m
+++ supporting_code/MBS_dylib/mFiles/RandomForests/preprocess/script_preprocessing_pipeline.m
@@ -6,6 +6,126 @@
 % 5. create synthetic rotations
 % 6. crop all images to standard size (w=800, h=900)
 
+%% 0. generate label images - NOTE I HAVEN'T USED THIS IN THIS PIPELINE BEFORE.  PREVIOUSLY, I DID THIS STEP FIRST USING "SCRIPT_denseAnnotationFromGT_multiple.".  copied below...
+
+% read in ground truth labeling of somite "parts" and create label image for RF training
+
+%
+position_nums = [1:9];
+data_dir = pwd;
+
+[dataSet, fname_list] = buildFullDataSet(data_dir, position_nums);
+save('GT_labelling.mat','dataSet','position_nums','fname_list');
+
+%% read data table into useful variable with position information associated with each somite
+
+clear all,
+
+fname = 'GT_labelling.mat';
+load(fname);
+
+num_somites = 21;
+
+for emb_num = 1:length(position_nums),
+    
+    % re-assign points from data table to "pos variable"
+    for i = 1:num_somites,
+        
+        if i < num_somites,
+            indx = 5*(i-1) + [1:5,9:-1:7];
+        else
+            indx = 5*(i-1) + [1:5,8:-1:6];
+        end
+        
+        % shift indx by corresponding position #
+        indx = indx + 108*(emb_num-1);
+        
+        pos(:,:,i,emb_num) = dataSet(indx,[2:3])';                      % ERROR: NEED TO UPDATE POSITION TO RELEVANT EMBRYO (SHIFT BY 108 ROWS)
+        
+    end
+    
+end
+
+%% create label image
+
+% user parameters - CAREFUL, THESE SHOULD REMAIN CONSTANT FOR ALL TRAINING DATA
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+uncertainty_radius = 2;
+open_radius = 8;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+train_image = getFileNames(pwd, '.tif');
+train_image = imread(train_image{1});
+
+figure(1),
+imagesc(train_image);
+colormap('gray');
+
+for i = 1:size(pos,4),
+    
+    label_image = zeros(size(train_image));
+    [X, Y] = meshgrid([1:size(train_image,1)]);
+    
+    for s = 1:num_somites;
+        
+        % create mask corresponding to the ith somite
+        xv = pos(1,:,s,i)';
+        yv = pos(2,:,s,i)';
+        [IN, ON] = inpolygon(X, Y, [xv; xv(1)], [yv; yv(1)]);
+        IN = IN - ON;       % only accept points IN the polygon, not on the boundary
+        
+        % shrink mask
+        nhood = fspecial('disk', uncertainty_radius);
+        nhood = ceil(nhood);
+        IN = imerode(IN,nhood);
+        
+        % round regions
+        nhood = fspecial('disk', open_radius);
+        nhood = ceil(nhood);
+        IN = imopen(IN,nhood);
+        
+        % turn mask into correct label
+        IN = IN*s;
+        label_image = label_image + IN;        
+        
+    end
+    
+    % set the foreground
+    FG = label_image > 0;
+    
+    % dilate FG to find the background
+    nhood = fspecial('disk', 5*uncertainty_radius);
+    nhood = ceil(nhood);
+    BG = 1 - imdilate(FG,nhood);
+    
+    % replace "uncertain" regions with NaN values
+    mask = zeros(size(label_image));
+    mask = mask + BG + FG;
+    mask(find(mask == 0)) = NaN;
+    label_image = label_image .* mask;
+    
+    label_image = uint8(label_image);
+    bfsave(label_image, strcat('label_image_',fname_list{i}(9:15),fname_list{i}(19:23),'.tif'));
+    
+    clear X Y xv yv IN ON nhood s FG BG mask label_image
+    
+end
+
+%{
+
+% display
+figure(2),
+imagesc(label_image),
+colormap('jet')
+set(gca,'XTick',[],'YTick',[]),
+
+figure(3),
+imagesc(mask),
+colormap('gray'),
+set(gca,'XTick',[],'YTick',[]),
+
+%}
+
 %% 1. register all images against the first
 
 clear all
